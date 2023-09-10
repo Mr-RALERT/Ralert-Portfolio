8ddd77f17c969b5b3c8cf34a54230bacef6d0b82
Mr-RALERT <143440335+Mr-RALERT@users.noreply.github.com>
2023-09-10 10:23:57 +0100

    Update ralert port  main.js

--- js/main.js
+++ js/main.js
@@ -11,9 +11,9 @@ let ulSide = document.querySelectorAll(".page .side-bar ul li");
 ulSide.forEach((li) => {
     li.addEventListener("click",(e) => {
         if (e.target.dataset.tp) {
-            window.open(`https://mr-ralert.github.io/Ralert-Portfolio/${e.target.dataset.tp}`,"_self")
+            window.open(`../Ralert-Portfolio/${e.target.dataset.tp}`,"_self")
         }else {
-            window.open(`https://mr-ralert.github.io/Ralert-Portfolio/${e.target.parentElement.dataset.tp}`,"_self")
+            window.open(`../Ralert-Portfolio/${e.target.parentElement.dataset.tp}`,"_self")
         }
     })
 })
